364b58188837b99935ada1e35d5612756c041cc3
Joemn <Aruljoe37@gmail.com>
2021-04-14 22:20:43 +0530

    Initial

---
+++ server/src/routes/user.mjs
@@ -0,0 +1,9 @@
+import express from "express";
+
+const router = express.Router();
+
+router.post("/signIn", (req, res) => {});
+
+router.post("/signUp", (req, res) => {});
+
+export default router;
---
+++ server/src/server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+
+// Routes Imports
+import userRoute from "./routes/user.mjs";
+
+// Db connection
+mongoose
+  .connect("mongodb://127.0.0.1:27017", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((err) => {
+    console.log("DB Connected");
+  });
+
+// Express Configs / Middleswares
+
+const app = express();
+dotenv.config();
+app.use(bodyParser.json());
+app.use("/api", userRoute);
+
+app.listen(process.env.PORT, () => {
+  console.log("Server is running");
+});
